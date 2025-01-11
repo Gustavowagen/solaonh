fafe0773f19bc79c35736c64fea4b2bcd2c0f610
Bjørnar Berge <bjornar.berge.07@gmail.com>
2025-01-11 12:07:28 +0100

    changes

--- src/.eslint.js
+++
@@ -1,5 +0,0 @@
-export const rules = {
-    "jsx-a11y/alt-text": "off", // Disable missing alt attributes warnings
-    "no-unused-vars": "off", // Disable unused variables warnings
-    "jsx-a11y/anchor-is-valid": "off", // Disable invalid anchor warnings
-};
